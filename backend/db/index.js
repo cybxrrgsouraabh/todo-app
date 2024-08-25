709ae794f95853cc386c96310d5ccae3c4fb798f
Sourabh Singh Bisht <142785399+cybxrrgsouraabh@users.noreply.github.com>
2024-08-25 13:55:17 +0530

    All basics things are done still more improvements to make

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://cyybxrg_p:cyybxrg7417@cluster0.fpt7cvq.mongodb.net/Todo-App");
+mongoose.connect("mongodb+srv://cyybxrg_p:<password>@cluster0.fpt7cvq.mongodb.net/Todo-App");
 
 const todoSchema = new mongoose.Schema({
     title: String,
